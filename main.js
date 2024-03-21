32db28c97c59a4afc18178371dff405b7ad55b88
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2024-03-20 22:22:02 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,69 @@
 console.log("Hola");
 let modal = document.getElementById('modal');
 let muneco = document.getElementById('muneco');
-let mensajes = document.getElementById('mensaje');
-let mensaje2 = document.getElementById("texto2");
-let btn_encriptar = document.getElementById('Encriptar');
-let btn_desencriptar = document.getElementById('Desencriptar');
+const btnEncriptar = document.getElementById('Encriptar');
+const btnDesencriptar = document.getElementById('Desencriptar');
+const mensaje2 = document.getElementById('texto2');
+const mensajes = document.getElementById('mensaje');
 
-
-mensajes.innerHTML = 'Ningun mensaje fue encontrado';
-
-
-btn_encriptar.addEventListener( "click", ()=>{
-  msg_encr = mensaje2.value.replace('e','enter').replace('i','imes').replace('a','ai').replace('o','ober').replace('u','ufat')
+btnEncriptar.addEventListener('click', () => {
+  const msgEncr = mensaje2.value.replace(/[aeiou]/g, (vocal) => {
+    switch (vocal) {
+      case 'a':
+        return 'ai';
+      case 'e':
+        return 'enter';
+      case 'i':
+        return 'imes';
+      case 'o':
+        return 'ober';
+      case 'u':
+        return 'ufat';
+      default:
+        return vocal;
+    }
+  });
   mensajes.innerHTML = '';
-  mensajes.innerHTML = msg_encr;
-  muneco.style.display = 'none'
-  console.log(msg_encr);
-  mensajes.style.left = '5%'
-  mensajes.style.right = '25%'
-  mensajes.style.bottom = '41%'
-  mensajes.style.alignContent  = 'center'
-  mensajes.style.justifyContent  = 'center'
-  mensajes.style.fontSize  = '20px'
-  mensajes.style.fontWeight  = '200'
-  mensajes.style.color  = '#343a40a1'
-})
+  mensajes.innerHTML = msgEncr;
+  muneco.style.display = 'none';
+  console.log(msgEncr);
+  mensajes.style.left = '5%';
+  mensajes.style.right = '25%';
+  mensajes.style.bottom = '41%';
+  mensajes.style.alignContent = 'center';
+  mensajes.style.justifyContent = 'center';
+  mensajes.style.fontSize = '20px';
+  mensajes.style.fontWeight = '200';
+  mensajes.style.color = '#343a40a1';
+});
 
-btn_desencriptar.addEventListener( "click", ()=>{
-  msg_encr = mensaje2.value.replace('enter','e').replace('imes','i').replace('ai','a').replace('ober','o').replace('ufat','u')
+btnDesencriptar.addEventListener('click', () => {
+  const msgEncr = mensaje2.value.replace(/[aiouefat]/g, (vocal) => {
+    switch (vocal) {
+      case 'ai':
+        return 'a';
+      case 'enter':
+        return 'e';
+      case 'imes':
+        return 'i';
+      case 'ober':
+        return 'o';
+      case 'ufat':
+        return 'u';
+      default:
+        return vocal;
+    }
+  });
   mensajes.innerHTML = '';
-  mensajes.innerHTML = msg_encr;
-  muneco.style.display = 'none'
-  console.log(msg_encr);
-  mensajes.style.left = '5%'
-  mensajes.style.right = '25%'
-  mensajes.style.bottom = '41%'
-  mensajes.style.alignContent  = 'center'
-  mensajes.style.justifyContent  = 'center'
-  mensajes.style.fontSize  = '20px'
-  mensajes.style.fontWeight  = '200'
-  mensajes.style.color  = '#343a40a1'
-})
+  mensajes.innerHTML = msgEncr;
+  muneco.style.display = 'none';
+  console.log(msgEncr);
+  mensajes.style.left = '5%';
+  mensajes.style.right = '25%';
+  mensajes.style.bottom = '41%';
+  mensajes.style.alignContent = 'center';
+  mensajes.style.justifyContent = 'center';
+  mensajes.style.fontSize = '20px';
+  mensajes.style.fontWeight = '200';
+  mensajes.style.color = '#343a40a1';
+});
